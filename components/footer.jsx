087db57237fa1e12c772a02055b5eaedcee6a7e2
daffaromero <daffaromero@mail.ugm.ac.id>
2023-08-08 17:55:09 +0700

    Fixed build error(s)

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className='bg-gray-950 px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen md:px-24 lg:px-8'>
       <div className='grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4'>
         <div className='sm:col-span-2'>
-          <a
+          <Link
             href='/'
             aria-label='keikeien'
             title='KKN-PPM UGM Samigaluh 2023'
@@ -29,7 +30,7 @@ const Footer = () => {
             <span className='ml-2 text-xl font-bold tracking-wide text-gray-300 uppercase'>
               KKN-PPM UGM Samigaluh 2023
             </span>
-          </a>
+          </Link>
           <div className='mt-6 lg:max-w-sm'>
             <p className='text-md text-gray-300'>
               Tim KKN-PPM UGM Samigaluh 2023
@@ -47,18 +48,18 @@ const Footer = () => {
           <div className='flex'></div>
           <div className='flex'>
             <p className='mr-1 text-gray-300 font-semibold'>Email:</p>
-            <a
+            <Link
               href='mailto:kkn@ugm.ac.id'
               aria-label='KKN email'
               title='KKN email'
               className='transition-colors duration-300 text-gray-300 hover:text-emerald-500'
             >
               kkn@ugm.ac.id
-            </a>
+            </Link>
           </div>
           <div className='flex'>
             <p className='mr-1 text-gray-300 font-semibold'>Address:</p>
-            <a
+            <Link
               href='https://goo.gl/maps/Sr7VoBUJaCqpzYXY9'
               target='_blank'
               rel='noopener noreferrer'
@@ -68,7 +69,7 @@ const Footer = () => {
             >
               69GG+6QP, Jl. Pancasila, Sagan, Caturtunggal, Kec. Depok,
               Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281
-            </a>
+            </Link>
           </div>
         </div>
         <div>
@@ -76,15 +77,15 @@ const Footer = () => {
             Social
           </span>
           <div className='flex items-center mt-1 space-x-3'>
-            <a
+            <Link
               href='/'
               className='text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400'
             >
               <svg viewBox='0 0 24 24' fill='currentColor' className='h-5'>
                 <path d='M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z' />
               </svg>
-            </a>
-            <a
+            </Link>
+            <Link
               href='https://instagram.com/kknsamigaluh'
               className='text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400'
             >
@@ -92,15 +93,15 @@ const Footer = () => {
                 <circle cx='15' cy='15' r='4' />
                 <path d='M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z' />
               </svg>
-            </a>
-            <a
+            </Link>
+            <Link
               href='/'
               className='text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-400'
             >
               <svg viewBox='0 0 24 24' fill='currentColor' className='h-5'>
                 <path d='M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z' />
               </svg>
-            </a>
+            </Link>
           </div>
           <p className='mt-4 text-sm text-gray-500'>
             KKN-PPM UGM adalah program pengabdian masyarakat yang dilaksanakan
@@ -115,28 +116,28 @@ const Footer = () => {
         </p>
         <ul className='flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row'>
           <li>
-            <a
+            <Link
               href='/'
               className='text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400'
             >
               F.A.Q
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href='/'
               className='text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400'
             >
               Privacy Policy
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href='/'
               className='text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400'
             >
               Terms &amp; Conditions
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
