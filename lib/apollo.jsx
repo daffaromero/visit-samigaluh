6add032a69d4b137f7beaaffeebe705e9d7dbc6c
daffaromero <daffaromero@mail.ugm.ac.id>
2023-08-08 20:52:53 +0700

    Changed env vars

--- lib/apollo.jsx
+++ lib/apollo.jsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://visitsamigaluh.local/graphql",
+  uri: process.env.WORDPRESS_API_URL,
   cache: new InMemoryCache(),
 });
 
